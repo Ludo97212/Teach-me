a9bd36a3f544b73818659e282b4c75284c3c8d31
Lina NEGOUAI <lina.negouai@wanadoo.fr>
2021-12-16 14:06:58 -0400

    ajout de 2 lectures à la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Lecture.create(name: "La gamme majeure", description: "Notions de base", price: 14.99, category_id: 6)
+Lecture.create(name: "Word", description: "Utilisation duntraitement de texte de Windows", price: 12.99, category_id: '')
