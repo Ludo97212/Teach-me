e5f4f3ff8197cb27e03fb78cd6e546de4d1e9156
Ludovic Nourel <ludovic.nourel@gmail.com>
2021-12-16 21:09:37 -0400

    routes modifiees?

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'categories#index'
-  resources :categories, only: [:index, :show]
-  resources :lectures, only: [:index, :show, :new, :create] do
-    resources :paniers, only: [:show, :new, :create]
+  devise_for :users do
+    resources :categories, only: [:index, :show]
+    resources :lectures do
+      resources :paniers, only: [:show, :new, :create]
+    end
   end
+
+  resources :categories, only: [:index, :show]
+  resources :lectures, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
