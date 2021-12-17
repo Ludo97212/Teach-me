2ef7a37f5219153f01bd2bdd07e348052532e030
Ludovic Nourel <ludovic.nourel@gmail.com>
2021-12-17 14:59:25 -0400

    navbar basique, seeds en comentaires

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
- paniers#new
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Lecture.create(name: "La gamme majeure", description: "Notions de base", price: 14.99, category_id: 6)
-Lecture.create(name: "Word", description: "Utilisation duntraitement de texte de Windows", price: 12.99, category_id: 4)
-=======
 
+# Lecture.destroy_all
+# Lecture.create(name: "La gamme majeure", description: "Notions de base", price: 14.99, category_id: 6)
+# Lecture.create(name: "Word", description: "Utilisation duntraitement de texte de Windows", price: 12.99, category_id: 7)
+# Lecture.create(name: "test", description: "Notions", price: 14.99, category_id: 46)
 
 # file = File.open('app/assets/images/.jpg')
 # category = Category.new(name: '')
@@ -57,5 +52,3 @@ file = File.open('app/assets/images/Divers.jpg')
 category = Category.new(name: 'Divers / Autres')
 category.photo.attach(io: file, filename: 'Divers.jpg', content_type: 'image/png')
 category.save
-
-
