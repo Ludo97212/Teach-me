e28523ebf818498ce4c193d5d39c7936edf17044
Ludovic Nourel <ludovic.nourel@gmail.com>
2021-12-18 22:04:29 -0400

    ok

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -6,7 +6,7 @@ class LecturesController < ApplicationController
   def show
     @lecture = Lecture.find(params[:id])
   end
-  
+
   def new
     @lecture = Lecture.new
   end
@@ -14,13 +14,12 @@ class LecturesController < ApplicationController
   def create
     @lecture = Lecture.new(lecture_params)
     @lecture.category_id = @category_id
-    @lecture.save
     redirect_to root_path
   end
 
   private
 
   def lecture_params
-    params.require(:lecture).permit(:name, :description, :price)
+    params.require(:lecture).permit(:name, :description, :price, :category, :photo)
   end
 end
