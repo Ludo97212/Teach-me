f2c014b690a64e79360de6c8a90689f13ea06435
Ludovic Nourel <ludovic.nourel@gmail.com>
2021-12-21 12:06:24 -0400

    ok

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -21,7 +21,7 @@ class LecturesController < ApplicationController
 
   def lecture_params
     params.require(:lecture)
-          .permit(:thematic, :name, :description, :public, :prerequis, :educational_objective,
+          .permit(:avatar, :first_name, :last_name, :thematic, :description, :public, :prerequis, :educational_objective,
                   :mean, :program, :evaluation, :price, :category_id, :photo)
   end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -3,5 +3,6 @@ class Lecture < ApplicationRecord
   has_many :paniers
   has_many :users, through: :paniers
   has_one_attached :photo
+  has_one_attached :avatar
   validates :name, :description, :price, :category, presence: true
 end
